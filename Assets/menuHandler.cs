6214078a027f45759d095f8e596f56de848289a4
ModSobGaming <modsobgaming@gmail.com>
2016-09-01 11:03:50 -0400

    Added basic Esc menu functionality:      Buffered input for Esc button.      Can summon/dismiss menu with Esc button in any scene.      'Resume Game' button will dismiss Esc menu.      'Settings' button will send user to setScene.      'Quit Game' button will send user to testScene (later mainMenuScene).
    Player placeholder objects and scripts created.
         'Player' empty game object will be the player reference.
         'playerControl' script will read from Input and convert it into vector information.
         'playerMove' script will take vector information and apply it to the player's velocity.

--- Assets/menuHandler.cs
+++ Assets/menuHandler.cs
@@ -1,15 +1,40 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
-public class menuHandler : MonoBehaviour {
+public class menuHandler : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public Canvas canvas;
+    bool buf;
+
+    // Use this for initialization
+    void Start()
+    {
+        buf = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape) && !buf)
+        {
+            canvas.enabled = !canvas.isActiveAndEnabled;
+            buf = true;
+        }
+        if(!Input.GetKey(KeyCode.Escape))
+        {
+            buf = false;
+        }
+    }
+
+    public void sceneChange(int _i)
+    {
+        SceneManager.LoadScene(_i);
+    }
+
+    public void closeMenu()
+    {
+        canvas.enabled = !canvas.isActiveAndEnabled;
+    }
 }
