40d9019e208bfd86ea1d3b1c9fb6c1382bbca22c
ModSobGaming <modsobgaming@gmail.com>
2016-09-09 20:42:32 -0400

    Added stealth functionality      Player mesh alpha is LERPed based on the distance between them and the nearby player.      Does not account for multiple nearby players (add competing values?)      Has not been tested in multiplayer.

---
+++ Assets/Scripts/playerStealth.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerStealth : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+    }
+
+    void OnTriggerStay(Collider _col)
+    {
+        if (_col.tag == "Finish")
+        {
+            float distance = (Vector3.Distance(transform.position, _col.transform.position) / GetComponent<SphereCollider>().radius);
+            Renderer[] rends = gameObject.GetComponentsInChildren<Renderer>();
+            foreach (Renderer rend in rends)
+            {
+                Color color;
+                foreach (Material mat in rend.materials)
+                {
+                    color = mat.color;
+                    color.a = Mathf.Lerp(1f, 0f, distance);
+                    mat.color = color;
+                }
+            }
+        }
+    }
+}
